4cec6ca9d8ea897eb063be08885d9359c88288f3
Sau Juliana <julienyange@gmail.com>
2022-01-20 20:52:47 +0300

    updated backend functionalities

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require(".routes/userRoutes");
 
 const app = express();
 dotenv.config();
@@ -12,15 +13,17 @@ app.get("/", (req, res) => {
   res.send("API is Running Successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id)
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+//   // console.log(req.params.id)
+//   const singleChat = chats.find((chat) => chat._id === req.params.id);
+//   res.send(singleChat);
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server has started on port ${PORT}`.yellow.bold));
