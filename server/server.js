87acb4ff28a7a0589a65efda2ffae22ca16c47ec
Sau Juliana <julienyange@gmail.com>
2022-01-20 19:04:24 +0300

    Connected express to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
-    res.send("API is Running Successfully");
-})
+  res.send("API is Running Successfully");
+});
 
 app.get("/api/chat", (req, res) => {
-    res.send(chats);
-})
+  res.send(chats);
+});
 
 app.get("/api/chat/:id", (req, res) => {
-    // console.log(req.params.id)
-    const singleChat = chats.find(chat => chat._id === req.params.id);
-    res.send(singleChat);
-
-})
+  // console.log(req.params.id)
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server has started on port ${PORT}`))
+app.listen(PORT, console.log(`Server has started on port ${PORT}`.yellow.bold));
