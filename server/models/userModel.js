32e556e6e207b34c6892e97a3fa767c3c4595980
Sau Juliana <julienyange@gmail.com>
2022-01-21 14:23:39 +0300

    created api for login and registration. Did password encryption and error handling

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,21 +1,38 @@
-const mongoose =  require("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true },
-        password: { type: String, required: true },
-        profile_pic: { 
-            type: String, 
-            required: true, 
-            default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" 
-        },
-
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profile_pic: {
+      type: String,
+      // required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    {
-        timestamps: true,
-    }
-)    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//logic to match password to the one in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//next is a middleware
+//logic to encrypt a password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- server/server.js
+++ server/server.js
@@ -3,17 +3,22 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
-const userRoutes = require(".routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
-const app = express();
 dotenv.config();
 connectDB();
+
+const app = express();
+app.use(express.json()); //to accept json data
 
 app.get("/", (req, res) => {
   res.send("API is Running Successfully");
 });
 
 app.use("/api/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 // app.get("/api/chat", (req, res) => {
 //   res.send(chats);
